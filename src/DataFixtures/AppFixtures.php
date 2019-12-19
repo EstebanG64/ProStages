0975a627d57eebf2885eec7748e1b72b8a036a81
Esteban Gutierrez <egutierre001@iutbayonne.univ-pau.fr>
2019-12-19 10:46:56 +0100

    Fin de la génération des données de tests

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Formation;
 use App\Entity\Entreprise;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
@@ -28,11 +29,15 @@ class AppFixtures extends Fixture
         $formationDUTIC->setNom("DUT TIC");
         $formationDUTIC->setNomLong("Diplome universitaire des technologies de l'information et de la communication");
 
+        $tabFormations = array($formationDUTInfo,$formationDUTIC,$formationLPProg);
+
         $manager->persist($formationDUTInfo);
         $manager->persist($formationDUTIC);
         $manager->persist($formationLPProg);
 
+        
         //Création des entreprises
+        $tabEntreprises = array();
         for($i = 0; $i < 5 ; $i++)
         {
             $entreprise = new Entreprise();
@@ -43,9 +48,63 @@ class AppFixtures extends Fixture
             $entreprise->setAdresse($faker->address());
             $entreprise->setActivite($faker->jobtitle());
             $entreprise->setSite($nomSansEspaces.$faker->regexify('\.(fr|es|com)'));
+            array_push($tabEntreprises,$entreprise);
             $manager->persist($entreprise);
 
         }
+
+        //Création des stages
+        foreach($tabEntreprises as $stageEntr)
+        {
+
+            for ($i=0; $i < 3; $i++)
+            {
+                $stage = new Stage();
+                $nbFormations = $faker->numberBetween(1,3); // Génération d'un nombre aléatoire pour définir le nombre de formations concernées apr le stage
+                switch($nbFormations)
+                {
+                    case 1:
+                    $choixFormation = $faker->numberBetween(0,2); // Génération d'un nombre aléatoire pour définir la formation qui concernera le stage
+                    $stage->addFormation($tabFormations[$choixFormation]);
+                    $tabFormations[$choixFormation]->addStage($stage);
+                    $manager->persist($tabFormations[$choixFormation]);
+
+                    break;
+                    case 2:
+                    $choixFormation1 = $faker->numberBetween(0,2); // Génération de deux nombres aléatoire pour définir les formations qui concerneront le stage
+                    $choixFormation2 = $faker->numberBetween(0,2);
+                    while($choixFormation1 == $choixFormation2) // Si les nombres générés sont identiques, en regénère un jusqu'à ce qu'il soit différent
+                    {
+                        $choixFormation2 = $faker->numberBetween(0,2);
+                    }
+                    $stage->addFormation($tabFormations[$choixFormation1]);
+                    $tabFormations[$choixFormation1]->addStage($stage);
+                    $stage->addFormation($tabFormations[$choixFormation2]);
+                    $tabFormations[$choixFormation2]->addStage($stage);
+                    $manager->persist($tabFormations[$choixFormation1]);
+                    $manager->persist($tabFormations[$choixFormation2]);
+                    break;
+
+                    case 3:
+                    foreach($tabFormations as $uneFormation)
+                    {
+                        $stage->addFormation($uneFormation);
+                        $uneFormation->addStage($stage);
+                        $manager->persist($uneFormation);
+                    }
+                    break;
+                }
+
+                $stage->setTitre($faker->jobtitle());
+                $stage->setMail("contact".$faker->firstname()."@".$stageEntr->getsite());
+                $stage->setDescription($faker->realtext(255));
+                $stage->setEntreprise($stageEntr);
+                $stageEntr->addStage($stage);
+                $manager->persist($stageEntr);
+                $manager->persist($stage);
+            }
+        }
+
         $manager->flush();
     }
 }
