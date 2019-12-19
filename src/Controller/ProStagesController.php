34115764191aa7a86cf41d391ca24d0b0d7144bb
Esteban Gutierrez <egutierre001@iutbayonne.univ-pau.fr>
2019-12-19 16:25:13 +0100

    Mise en place des pages stages par formations, stages par entreprises, et bonne mise en place des données de tests sur toutes les pages

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -5,27 +5,55 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Stage;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+
 class ProStagesController extends AbstractController
 {
 
     public function index()
     {
-        return $this->render('pro_stages/index.html.twig');
+        $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stages=$repositoryStage->findall();
+        return $this->render('pro_stages/index.html.twig',['stages'=>$stages]);
     }
     public function entreprises()
     {
-        return $this->render('pro_stages/entreprises.html.twig');
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+        $entreprises=$repositoryEntreprise->findall();
+        return $this->render('pro_stages/entreprises.html.twig',['entreprises'=>$entreprises]);
 
     }
     public function formations()
     {
-        return $this->render('pro_stages/formations.html.twig');
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+        $formations=$repositoryFormation->findall();
+        return $this->render('pro_stages/formations.html.twig',['formations'=>$formations]);
 
     }
     public function stages($id)
     {
+        $repoStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stage = $repoStage->find($id);
         return $this->render('pro_stages/stages.html.twig',
-        ['idStage'=>$id]);
+        ['stage'=>$stage]);
+
+    }
+    public function entreprise($id)
+    {
+        $repoStage = $this->getDoctrine()->getRepository(Stage::class);
+        $stage = $repoStage->findByEntreprise($id);
+        return $this->render('pro_stages/entreprise.html.twig',
+        ['stages'=>$stage]);
+
+    }
+    public function formation($id)
+    {
+        $repoFormation = $this->getDoctrine()->getRepository(Formation::class);
+        $formation = $repoFormation->find($id);
+        return $this->render('pro_stages/formation.html.twig',
+        ['formation'=>$formation]);
 
     }
 }
